523d77abdee17a127a76cd36a797031709090d96
Avairon <75504999+Avairon@users.noreply.github.com>
2023-12-29 17:20:18 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,31 +9,11 @@ using std::string;
 using std::map;
 using std::vector;
 
-map <int, string> start_arab(){
-    map <int, string> arab_out;
-    
-    arab_out[0] = "Приз 100 рублей!";
-    arab_out[1] = "Приз 150 рублей!";
-    arab_out[2] = "Приз 200 рублей!";
-    arab_out[3] = "Приз 250 рублей!";
-    arab_out[4] = "Приз 300 рублей!";
-    arab_out[5] = "СУПЕРПРИЗ! Награда 500 рублей!";
-    
-    return arab_out;
-}
+int chances_arab[6] = {14, 6, 4, 3, 2, 1};
+int chances_bitch[6] = {12, 7, 5, 4, 3, 2}; //{12, 7, 5, 3, 2, 1};
 
-map <int, string> start_bitch(){
-    map <int, string> bitch_out;
-    
-    bitch_out[0] = "Приз 10 рублей!";
-    bitch_out[1] = "Приз 20 рублей!";
-    bitch_out[2] = "Приз 30 рублей!";
-    bitch_out[3] = "Приз 50 рублей!";
-    bitch_out[4] = "Приз 70 рублей!";
-    bitch_out[5] = "СУПЕРПРИЗ 100 рублей!";
-    
-    return bitch_out;
-}
+std::random_device rd; // obtain a random number from hardware
+std::mt19937 gen(rd());
 
 vector<int> chances_buff_gen(int* chances){
     vector<int> chances_buff;
@@ -45,6 +25,8 @@ vector<int> chances_buff_gen(int* chances){
         chances_buff.push_back(chances[0] + chances[1] + chances[2] + chances[3] + chances[4] + chances[5]);
     return chances_buff;
 }
+
+int buff[3] = {0, chances_buff_gen(chances_bitch)[5], chances_buff_gen(chances_arab)[5]};
 
 int roll(int* chances, int luck_input){
     int chances_buff[6];
@@ -63,62 +45,100 @@ int roll(int* chances, int luck_input){
     return luck_output;
 }    
 
+class member{
+private:
+    int money;
+    int rolls;
+    
+    map <int, int> arab_prices;
+    map <int, int> bitch_prices;
+    
+public:
+    member(){};
+    member(int Rolls, int Money){
+        rolls = Rolls;
+        money = Money;
+    }
+    void begin(){
+        arab_prices[0] = 100;
+        arab_prices[1] = 150;
+        arab_prices[2] = 200;
+        arab_prices[3] = 250;
+        arab_prices[4] = 300;
+        arab_prices[5] = 500;
+    
+        bitch_prices[0] = 10;
+        bitch_prices[1] = 20;
+        bitch_prices[2] = 30;
+        bitch_prices[3] = 40;
+        bitch_prices[4] = 50;
+        bitch_prices[5] = 100;
+    }
+    void roll_arab(){
+        if(rolls > 0){
+            rolls-=1;
+            std::uniform_int_distribution<> distr(0, buff[2]); // define the range
+            int luck_input = distr(gen);
+        
+            cout << "Вы выйграли: " << arab_prices[roll(chances_arab, luck_input)] << " рублей!\n";
+            money += arab_prices[roll(chances_arab, luck_input)];
+        }
+        else{
+            cout << "У вас нет круток в АААРАБСКОЕ казино!";
+        }
+        
+    }
+    void roll_bitch(){
+        if(rolls > 0){
+            rolls-=1;
+            std::uniform_int_distribution<> distr(0, buff[1]); // define the range
+            int luck_input = distr(gen);
+        
+            cout << "Вы выйграли: " << arab_prices[roll(chances_bitch, luck_input)] << " рублей!\n";
+            money += bitch_prices[roll(chances_bitch, luck_input)];
+        }
+        else{
+            cout << "У вас нет круток в наноказик!";
+        }
+    }
+    void info(){
+        cout << "Награды нашей рулетки!\n";
+        cout << "\n";
+    
+        cout << "Награды наноказика\n";
+        for (int i = 0; i <= 5; i++) cout << bitch_prices[i] << " рублей, шанс: " << chances_bitch[i] * 100 / chances_buff_gen(chances_bitch)[5] << "%\n";
+        cout << "\n";
+    
+        cout << "Награды АААРАБСКОГО казино\n";
+        for (int i = 0; i <= 5; i++) cout << arab_prices[i] << " рублей, шанс: " << chances_arab[i] * 100 / chances_buff_gen(chances_arab)[5] << "%\n";
+        cout << "\n";
+    }
+};
+
 int main()
 {
     setlocale(LC_ALL, "Russian");
     
-    std::random_device rd; // obtain a random number from hardware
-    std::mt19937 gen(rd()); // seed the generator
-    //std::uniform_int_distribution<> distr(0, 30); // define the range
-    
-    map <int, string> nag_arab;
-    map <int, string> nag_bitch;
-    
-    nag_arab = start_arab();
-    nag_bitch = start_bitch();
-    
-    
-    
-    int chances_arab[6] = {14, 6, 4, 3, 2, 1};
-    int chances_bitch[6] = {12, 7, 5, 4, 3, 2}; //{12, 7, 5, 3, 2, 1};
-    
-    /*DEBUG
-    for(int i = 0; i <= 5; i++) cout << chances_buff[i] << "\n";
-    
-    cout << "\n"; cout << "\n";
-    */
-    
     int choose = 0;
     
-    cout << "Награды нашей рулетки!\n";
-    cout << "\n";
     
-    cout << "Награды наноказика\n";
-    for (int i = 0; i <= 5; i++) cout << nag_bitch[i] << " шанс: " << chances_bitch[i] * 100 / chances_buff_gen(chances_bitch)[5] << "%\n";
-    cout << "\n";
-    
-    cout << "Награды АААРАБСКОГО казино\n";
-    for (int i = 0; i <= 5; i++) cout << nag_arab[i] << " шанс: " << chances_arab[i] * 100 / chances_buff_gen(chances_arab)[5] << "%\n";
-    cout << "\n";
-    cout << "\n";
+    member user = *new member(1, 0);
+    user.begin();
     
     cout << "Выберите казино\n 1 - наноказик\n 2 - АААРАБСКОЕ казино\n";
     std::cin >> choose;
     
-    int buff[3] = {0, chances_buff_gen(chances_bitch)[5], chances_buff_gen(chances_arab)[5]};
     
-    std::uniform_int_distribution<> distr(0, buff[choose]); // define the range
-    int luck_input = distr(gen);
     
     switch(choose){
         case(1):
-            cout << nag_bitch[roll(chances_bitch, luck_input)];
+            user.roll_bitch();
             break;
         case(2):
-            cout << nag_arab[roll(chances_arab, luck_input)];
+            user.roll_arab();
             break;
         default:
-            cout << "Неверный ввод! Перезапустите код";
+            user.info();
             break;
     }
 }
